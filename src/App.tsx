f0f8bdba913a9df21d034add2cb60da91008d7b4
Justyna Luc <justiluc@interia.pl>
2023-04-23 22:39:09 +0200

    Image Tile Comoponent + with hardcoded tile values, will be changed later

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import './style.css';
 import TextTile from './TextTileComponent.tsx';
+import  ImageTile  from './ImageTileComponent.tsx';
 
 export default function App() {
   // load definition here
-  const tile = {
+  const textTile = {
     "type": "textTile",
     "elementKey": "641962fe-f92a-4993-ba75-22882eff59d5",
     "title": "Who is my kotecek?",
     "text": "Having a kotecek as a pet can be an incredibly rewarding experience. They are affectionate animals that provide companionship and entertainment. Koteceks are known for their playful nature and can provide hours of entertainment with their antics.",
     "color": "mid"
+  }
+  const imageTile = {
+    "type": "imageTile",
+    "elementKey": "1e07a55e-c594-46f7-a1eb-cbec3295e092",
+    "source": "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "title": "Kitty"
   }
 
   return (
@@ -17,7 +24,8 @@ export default function App() {
       {/* TODO remove title usage from template */}
       <h1>{'Place you components here 👇'}</h1>
       <div className="content">
-        <TextTile tile = {tile}></TextTile>
+        {/* <TextTile tile = {textTile}></TextTile> */}
+        <ImageTile tile = {imageTile}></ImageTile>
       </div>
     </div>
   );
---
+++ src/ImageTileComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import {ImageTile} from './model.ts'
+
+export default function ImageTileComponent(tile: ImageTile) {
+  // load definition here
+  
+  return (
+    <div className={tile.tile.type} key={tile.tile.elementKey}>
+        <img src={tile.tile.source} alt={tile.tile.title}/> 
+    </div>
+  );
+}
